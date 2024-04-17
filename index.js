5fb69f7bd6ea27991dea06c9cc20b996cbcd03a6
Francisco <francardoso@outlook.com>
2024-04-17 15:12:40 -0300

    feat: INF-529 working lambda

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-const AWS = require("aws-sdk");
+const {
+  ResourceGroupsTaggingAPIClient,
+  TagResourcesCommand,
+} = require("@aws-sdk/client-resource-groups-tagging-api");
 
-exports.handler = async (event, context) => {
-  // const client = new AWS.CloudTrail();
-  const resourceType = event.detail.configurationItem.resourceType;
+exports.handler = async (event) => {
   const resourceArn = event.resources[0];
-  const resourceTaggingApiClient = new AWS.ResourceGroupsTaggingAPI();
 
-  // TODO: Before the next stuff, just create the lambda as is and see if it works (First manually. Later will be automated)
-  // TODO: Validate if one of the supported resource types
-  // TODO: Validate if tag already does not exist (Or just don't fail the lambda in case it already exists)
+  // ResourceType filter is done at EvenBridge rule, so we don't need to redo it here.
+  console.log(`Requesting to tag ${resourceArn}...`);
 
-  response = await resourceTaggingApiClient.tagResources({
-    ResourceARNList: [resourceArn],
-    Tags: [{ Key: "Company", Value: "Sonder" }],
+  const taggingClient = new ResourceGroupsTaggingAPIClient({
+    region: "us-east-1",
   });
-  console.log(`${resourceArn} was tagged`);
+  const tagResourcesCommand = new TagResourcesCommand({
+    ResourceARNList: [resourceArn],
+    Tags: { Company: "Sonder" },
+  });
+
+  try {
+    const data = await taggingClient.send(tagResourcesCommand);
+    const responseStr = JSON.stringify(data);
+    console.log(`Response from tagResource command: ${responseStr}`);
+    if (
+      data.$metadata.httpStatusCode < 200 ||
+      data.$metadata.httpStatusCode > 299 ||
+      (data.failedResourcesMap &&
+        Object.keys(data.failedResourcesMap).length > 0)
+    ) {
+      throw new Error(responseStr);
+    }
+  } catch (error) {
+    console.log(`OnCatch error ${JSON.stringify(error)}`);
+    throw error;
+  }
 };
